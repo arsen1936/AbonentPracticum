2e71ccde93c0872fd0cc133d03feccb9b99f08cc
arsen1936 <artem.j2006@gmail.com>
2026-07-06 18:19:27 +0300

    Добавлен StringSorterService

--- src/backend/WebApp.Api/Program.cs
+++ src/backend/WebApp.Api/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddSingleton<IUtilityService, SumNumbersService>();
 builder.Services.AddSingleton<IUtilityService, HashCalculatorService>();
 builder.Services.AddSingleton<IUtilityService, NumberConverterService>();
 builder.Services.AddSingleton<IUtilityService, TextToListService>();
+builder.Services.AddSingleton<IUtilityService, StringSorterService>();
 // builder.Services.AddSingleton<IUtilityService, MyNewService>();  // ← пример для новой утилиты
 
 // --- Swagger ---
---
+++ src/backend/WebApp.Api/Services/StringSorterService.cs
@@ -0,0 +1,67 @@
+﻿namespace WebApp.Api.Services;
+
+public class StringSorterService : IUtilityService
+{
+    public string Endpoint => "string-sort";
+
+    public string Execute(string input)
+    {
+        var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        if (lines.Length >= 2)
+        {
+            return GetSortedLines(lines[0], lines[1..]);
+        }
+        else
+        {
+            throw new Exception("Input string was not in a correct format");
+        }
+    }
+
+    private string GetSortedLines(string sortingOrder, string[] lines)
+    {
+        string sortedLines = sortingOrder.ToLower() switch
+        {
+            "asc" => getASCLines(lines),
+            "desc" => getDECLines(lines),
+            _ => throw new Exception("Неправильно указан способ сортировки")
+        };
+        return sortedLines;
+    }
+
+    private string getASCLines(string[] lines)
+    {
+        var asc = new SortedSet<string>();
+        foreach (var line in lines)
+        {
+            if (line.Any(char.IsWhiteSpace))
+            {
+                throw new Exception("Строка должна содержать только одно слово.");
+            }
+            else
+            {
+                asc.Add(line);
+            }
+        }
+
+        return string.Join(", ", asc);
+    }
+
+    private string getDECLines(string[] lines)
+    {
+        var desc = new SortedSet<string>(
+            Comparer<string>.Create((a, b) => string.Compare(b, a)));
+        foreach (var line in lines)
+        {
+            if (line.Any(char.IsWhiteSpace))
+            {
+                throw new Exception("Строка должна содержать только одно слово.");
+            }
+            else
+            {
+                desc.Add(line);
+            }
+        }
+
+        return string.Join(", ", desc);
+    }
+}
